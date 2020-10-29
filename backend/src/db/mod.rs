9ce7cf05c3f1a14c366c1ef3787c9cfd33173e6d
Rolf Kreibaum <rolf.kreibaum@posteo.de>
2020-10-29 22:53:20 +0100

    Use sqlx for the database, fix #12

---
+++ backend/src/db/mod.rs
@@ -0,0 +1,146 @@
+use super::{LoginRequest, Position, SavePositionRequest, SavePositionResponse, ServerError, User};
+/// All database logic for the pacosako game server lives in this project.
+/// We are using sqlx to talk to an sqlite database.
+use sqlx::sqlite::SqlitePool;
+
+pub struct Pool(SqlitePool);
+
+struct PositionRaw {
+    id: i64,
+    owner: i64,
+    data: Option<String>,
+}
+
+impl From<sqlx::Error> for ServerError {
+    fn from(db_error: sqlx::Error) -> Self {
+        ServerError::DatabaseError {
+            message: db_error.to_string(),
+        }
+    }
+}
+
+impl Pool {
+    pub async fn new(database_path: &str) -> Result<Self, sqlx::Error> {
+        let pool = SqlitePool::connect(database_path).await?;
+
+        Ok(Pool(pool))
+    }
+
+    pub(crate) async fn position_create(
+        &self,
+        request: SavePositionRequest,
+        user: User,
+    ) -> Result<SavePositionResponse, sqlx::Error> {
+        let mut conn = self.0.acquire().await?;
+
+        let request_data = request.data.to_string();
+
+        let board_id = sqlx::query!(
+            "INSERT INTO position (owner, data) VALUES (?1, ?2)",
+            user.user_id,
+            request_data,
+        )
+        .execute(&mut conn)
+        .await?
+        .last_insert_rowid();
+
+        Ok(SavePositionResponse { id: board_id })
+    }
+
+    pub(crate) async fn position_update(
+        &self,
+        board_id: i64,
+        request: SavePositionRequest,
+    ) -> Result<SavePositionResponse, sqlx::Error> {
+        let mut conn = self.0.acquire().await?;
+
+        let request_data = request.data.to_string();
+
+        sqlx::query!(
+            "UPDATE position SET data = ?1 WHERE id = ?2",
+            board_id,
+            request_data
+        )
+        .execute(&mut conn)
+        .await?;
+
+        Ok(SavePositionResponse { id: board_id })
+    }
+
+    pub(crate) async fn position_get(&self, id: i64) -> Result<Position, ServerError> {
+        let rec: Option<PositionRaw> = sqlx::query_as!(
+            PositionRaw,
+            "SELECT id, owner, data FROM position WHERE id = ?",
+            id
+        )
+        .fetch_optional(&self.0)
+        .await?;
+
+        if let Some(rec) = rec {
+            rec.into_position()
+        } else {
+            Err(ServerError::NotFound)
+        }
+    }
+
+    pub(crate) async fn position_get_list(
+        &self,
+        user_id: i64,
+    ) -> Result<Vec<Position>, ServerError> {
+        let recs: Vec<PositionRaw> = sqlx::query_as!(
+            PositionRaw,
+            "SELECT id, owner, data FROM position WHERE owner = ?1",
+            user_id
+        )
+        .fetch_all(&self.0)
+        .await?;
+
+        recs.iter().map(PositionRaw::into_position).collect()
+    }
+
+    pub(crate) async fn get_user(&self, username: String) -> Result<User, ServerError> {
+        Ok(sqlx::query_as!(
+            User,
+            "SELECT id as user_id, username FROM user WHERE username = ?1",
+            username
+        )
+        .fetch_one(&self.0)
+        .await?)
+    }
+
+    pub(crate) async fn check_password(&self, login: &LoginRequest) -> Result<bool, ServerError> {
+        use pbkdf2::pbkdf2_check;
+
+        let rec = sqlx::query!(
+            "SELECT password FROM user WHERE username = ?1",
+            login.username,
+        )
+        .fetch_one(&self.0)
+        .await?;
+
+        if let Some(hash) = rec.password {
+            Ok(pbkdf2_check(&login.password, &hash).is_ok())
+        } else {
+            // If the user has no password, they can't log in ever.
+            Ok(false)
+        }
+    }
+}
+
+impl PositionRaw {
+    fn into_position(&self) -> Result<Position, ServerError> {
+        Ok(Position {
+            id: self.id,
+            owner: self.owner,
+            data: json_parse(&self.data)?,
+        })
+    }
+}
+
+fn json_parse(raw: &Option<String>) -> Result<serde_json::Value, ServerError> {
+    if let Some(raw) = raw {
+        serde_json::from_str(raw).map_err(|_| ServerError::DeserializationFailed)
+    } else {
+        Err(ServerError::DeserializationFailed)
+    }
+}
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,6 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
+mod db;
 mod instance_manager;
 mod sync_match;
 mod timeout;
@@ -8,12 +9,14 @@ mod websocket;
 
 #[macro_use]
 extern crate rocket;
-#[macro_use]
-extern crate rocket_contrib;
 extern crate pbkdf2;
+extern crate rocket_contrib;
 #[macro_use]
 extern crate log;
 extern crate simplelog;
+use async_std::task;
+use async_std::task::block_on;
+use db::Pool;
 use pacosako::{DenseBoard, PacoError, SakoSearchResult};
 use rand::{thread_rng, Rng};
 use rocket::http::{Cookie, Cookies};
@@ -21,14 +24,10 @@ use rocket::outcome::IntoOutcome;
 use rocket::request::{self, FromRequest, Request};
 use rocket::response::{Flash, Redirect};
 use rocket::State;
-use rocket_contrib::databases::rusqlite;
 use rocket_contrib::json::Json;
 use serde::{Deserialize, Serialize};
 use std::fs::File;
 use websocket::WebsocketServer;
-
-#[database("sqlite_logs")]
-struct DbConn(rusqlite::Connection);
 
 ////////////////////////////////////////////////////////////////////////////////
 // Static Files ////////////////////////////////////////////////////////////////
@@ -63,14 +62,6 @@ enum ServerError {
     GameError { message: String },
     NotAllowed,
     NotFound,
-}
-
-impl From<rusqlite::Error> for ServerError {
-    fn from(db_error: rusqlite::Error) -> Self {
-        ServerError::DatabaseError {
-            message: db_error.to_string(),
-        }
-    }
 }
 
 impl From<ServerError> for Json<ServerError> {
@@ -111,27 +102,12 @@ struct SavePositionResponse {
 fn position_create(
     create_request: Json<SavePositionRequest>,
     user: User,
-    conn: DbConn,
+    pool: State<Pool>,
 ) -> Result<Json<SavePositionResponse>, String> {
-    match position_create_db(create_request.0, user, conn) {
+    match task::block_on(pool.position_create(create_request.0, user)) {
         Ok(v) => Ok(Json(v)),
         Err(e) => Err(e.to_string()),
     }
-}
-
-fn position_create_db(
-    request: SavePositionRequest,
-    user: User,
-    conn: DbConn,
-) -> Result<SavePositionResponse, rusqlite::Error> {
-    conn.execute(
-        "INSERT INTO position (owner, data) VALUES (?1, ?2)",
-        &[&user.user_id.to_string(), &request.data.to_string()],
-    )?;
-
-    let board_id = conn.last_insert_rowid();
-
-    Ok(SavePositionResponse { id: board_id })
 }
 
 #[post("/position/<id>", data = "<update_request>")]
@@ -139,27 +115,14 @@ fn position_update(
     id: i64,
     update_request: Json<SavePositionRequest>,
     user: User,
-    conn: DbConn,
+    pool: State<Pool>,
 ) -> Result<Json<SavePositionResponse>, Json<ServerError>> {
     // You can only update a position that you own.
-    if position_get_db(id, &conn)?.owner != user.user_id {
+    if task::block_on(pool.position_get(id))?.owner != user.user_id {
         return Err(Json(ServerError::NotAllowed));
     }
 
-    json_result(position_update_db(id, update_request.0, &conn))
-}
-
-fn position_update_db(
-    board_id: i64,
-    request: SavePositionRequest,
-    conn: &DbConn,
-) -> Result<SavePositionResponse, ServerError> {
-    conn.execute(
-        "UPDATE position SET data = ?1 WHERE id = ?2",
-        &[&board_id.to_string(), &request.data.to_string()],
-    )?;
-
-    Ok(SavePositionResponse { id: board_id })
+    json_result(task::block_on(pool.position_update(id, update_request.0)).map_err(|e| e.into()))
 }
 
 #[derive(Serialize)]
@@ -170,8 +133,12 @@ struct Position {
 }
 
 #[get("/position/<id>")]
-fn position_get(id: i64, user: User, conn: DbConn) -> Result<Json<Position>, Json<ServerError>> {
-    let position = position_get_db(id, &conn).and_then(|position| {
+fn position_get(
+    id: i64,
+    user: User,
+    conn: State<Pool>,
+) -> Result<Json<Position>, Json<ServerError>> {
+    let position = block_on(conn.position_get(id)).and_then(|position| {
         if position.owner == user.user_id {
             Ok(position)
         } else {
@@ -188,207 +155,13 @@ fn json_result<T, U>(result: Result<T, U>) -> Result<Json<T>, Json<U>> {
     }
 }
 
-fn position_get_db(id: i64, conn: &DbConn) -> Result<Position, ServerError> {
-    let stmt = "SELECT id, owner, data FROM position WHERE id = :id";
-
-    let mut stmt = conn.prepare(&stmt)?;
-    let position: Result<Position, ServerError> = stmt
-        .query_map_named(&[(":id", &id)], |row| {
-            let data: String = row.get(2);
-            Position {
-                id: row.get(0),
-                owner: row.get(1),
-                data: serde_json::from_str(&data).unwrap(),
-            }
-        })?
-        .nth(0)
-        .map_or(Err(ServerError::NotFound), |res| Ok(res?));
-
-    position
-}
-
 /// List all positions owned by the currently logged in user.
 #[get("/position")]
-fn position_get_list(user: User, conn: DbConn) -> Result<Json<Vec<Position>>, Json<ServerError>> {
-    json_result(position_get_list_db(user.user_id, &conn))
-}
-
-fn position_get_list_db(user_id: i64, conn: &DbConn) -> Result<Vec<Position>, ServerError> {
-    let stmt = "SELECT id, owner, data FROM position WHERE owner = :owner";
-
-    let mut stmt = conn.prepare(&stmt)?;
-    let positions: Result<Vec<Position>, ServerError> = stmt
-        .query_map_named(&[(":owner", &user_id)], |row| {
-            let data: String = row.get(2);
-            Position {
-                id: row.get(0),
-                owner: row.get(1),
-                data: serde_json::from_str(&data).unwrap(),
-            }
-        })?
-        .map(|position| Ok(position?)) // convert rusqlite::Error -> ServerError
-        .collect();
-
-    positions
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// Article CRUD ////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-
-#[derive(Serialize, Deserialize)]
-struct Article {
-    id: i64,
-    creator: i64,
-    title: String,
-    body: String,
-    visible: i64,
-}
-
-trait Crud: Sized {
-    fn save(self, conn: &DbConn) -> Result<Self, ServerError>;
-    fn load(id: i64, conn: &DbConn) -> Result<Self, ServerError>;
-    fn from_row(row: &rocket_contrib::databases::rusqlite::Row<'_, '_>) -> Self;
-}
-
-impl Crud for Article {
-    fn save(mut self, conn: &DbConn) -> Result<Self, ServerError> {
-        if self.id > 1 {
-            conn.execute(
-                "UPDATE article SET creator = ?1, title = ?2, body = ?3, visible = ?4 WHERE id = ?5",
-                &[
-                    &self.creator.to_string(),
-                    &self.title,
-                    &self.body,
-                    &self.visible,
-                    &self.id.to_string(),
-                ],
-            )?;
-        } else {
-            conn.execute(
-                "INSERT INTO article (creator, title, body, visible) VALUES (?1, ?2, ?3, ?4)",
-                &[
-                    &self.creator.to_string(),
-                    &self.title,
-                    &self.body,
-                    &self.visible,
-                ],
-            )?;
-            self.id = conn.last_insert_rowid();
-        }
-        Ok(self)
-    }
-    fn load(id: i64, conn: &DbConn) -> Result<Self, ServerError> {
-        let stmt = "select id, creator, title, body, visible from article where id = :id";
-        let mut stmt = conn.prepare(&stmt)?;
-        let article: Result<Article, ServerError> = stmt
-            .query_map_named(&[(":id", &id)], Article::from_row)?
-            .nth(0)
-            .map_or(Err(ServerError::NotFound), |res| Ok(res?));
-
-        article
-    }
-    fn from_row(row: &rocket_contrib::databases::rusqlite::Row<'_, '_>) -> Self {
-        Article {
-            id: row.get(0),
-            creator: row.get(1),
-            title: row.get(2),
-            body: row.get(3),
-            visible: row.get(4),
-        }
-    }
-}
-
-/// List all articles created by the currently logged in user.
-#[get("/article/my")]
-fn article_get_my_list(user: User, conn: DbConn) -> Result<Json<Vec<Article>>, Json<ServerError>> {
-    json_result(article_get_my_list_db(user.user_id, &conn))
-}
-
-fn article_get_my_list_db(user_id: i64, conn: &DbConn) -> Result<Vec<Article>, ServerError> {
-    let stmt = "select id, creator, title, body, visible from article where creator = :creator";
-
-    let mut stmt = conn.prepare(&stmt)?;
-    let positions: Result<Vec<Article>, ServerError> = stmt
-        .query_map_named(&[(":creator", &user_id)], Article::from_row)?
-        .map(|position| Ok(position?)) // convert rusqlite::Error -> ServerError
-        .collect();
-
-    positions
-}
-
-/// List all articles created by the currently logged in user.
-#[get("/article/public")]
-fn article_get_public_list(conn: DbConn) -> Result<Json<Vec<Article>>, Json<ServerError>> {
-    json_result(article_get_public_list_db(&conn))
-}
-
-fn article_get_public_list_db(conn: &DbConn) -> Result<Vec<Article>, ServerError> {
-    let stmt = "select id, creator, title, body, visible from article where visible = 1";
-
-    let mut stmt = conn.prepare(&stmt)?;
-    let positions: Result<Vec<Article>, ServerError> = stmt
-        .query_map_named(&[], Article::from_row)?
-        .map(|position| Ok(position?)) // convert rusqlite::Error -> ServerError
-        .collect();
-
-    positions
-}
-
-/// Returns a particular article, if this article exists and is visible to the
-/// logged in user.
-/// TODO: I should take an Option<User> to allow non-logged in users to access
-/// public articles.
-#[get("/article/<id>")]
-fn article_get(id: i64, user: User, conn: DbConn) -> Result<Json<Article>, Json<ServerError>> {
-    let article = Article::load(id, &conn).and_then(|article| {
-        if article.creator == user.user_id {
-            Ok(article)
-        } else {
-            Err(ServerError::NotAllowed)
-        }
-    });
-    json_result(article)
-}
-
-#[post("/article", data = "<article>")]
-fn article_post(
-    mut article: Json<Article>,
+fn position_get_list(
     user: User,
-    conn: DbConn,
-) -> Result<Json<Article>, Json<ServerError>> {
-    let new_article = if article.id <= 0 {
-        // Create a new article
-        article.creator = user.user_id;
-        article.0.save(&conn)
-    } else {
-        let old_article = Article::load(article.id, &conn)?;
-        // Check if the user owns article, then update
-        if old_article.creator == article.creator && old_article.creator == user.user_id {
-            article.0.save(&conn)
-        } else {
-            Err(ServerError::NotAllowed)
-        }
-    };
-
-    json_result(new_article)
-}
-
-#[post("/article/visible", data = "<article>")]
-fn article_post_visible(
-    article: Json<Article>,
-    user: User,
-    conn: DbConn,
-) -> Result<Json<Article>, Json<ServerError>> {
-    let mut old_article = Article::load(article.id, &conn)?;
-    let new_article = if old_article.creator == user.user_id {
-        old_article.visible = article.visible;
-        article.0.save(&conn)
-    } else {
-        Err(ServerError::NotAllowed)
-    };
-
-    json_result(new_article)
+    conn: State<Pool>,
+) -> Result<Json<Vec<Position>>, Json<ServerError>> {
+    json_result(block_on(conn.position_get_list(user.user_id)))
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -413,26 +186,15 @@ impl<'a, 'r> FromRequest<'a, 'r> for User {
     type Error = ();
 
     fn from_request(request: &'a Request<'r>) -> request::Outcome<User, ()> {
-        let conn = request.guard::<DbConn>()?;
+        let pool: State<Pool> = request.guard()?;
+
         request
             .cookies()
             .get_private("user_id")
             .map(|cookie| cookie.value().to_owned())
-            .and_then(|username| get_user(username.to_owned(), &conn).ok())
+            .and_then(|username| block_on(pool.get_user(username.to_owned())).ok())
             .or_forward(())
     }
-}
-
-fn get_user(username: String, conn: &DbConn) -> Result<User, rusqlite::Error> {
-    let stmt = "SELECT id, username FROM user WHERE username = :username";
-
-    let mut stmt = conn.prepare(&stmt)?;
-    let user_id: i64 = stmt
-        .query_map_named(&[(":username", &username)], |row| row.get(0))?
-        .nth(0)
-        .unwrap()?;
-
-    Ok(User { user_id, username })
 }
 
 /// Log in with username and password
@@ -440,32 +202,17 @@ fn get_user(username: String, conn: &DbConn) -> Result<User, rusqlite::Error> {
 fn login(
     login: Json<LoginRequest>,
     mut cookies: Cookies,
-    conn: DbConn,
+    conn: State<Pool>,
 ) -> Result<Json<User>, Json<ServerError>> {
-    match check_password(&login.0, &conn) {
+    match block_on(conn.check_password(&login.0)) {
         Ok(true) => {
             cookies.add_private(Cookie::new("user_id", login.username.clone()));
-            let user = get_user(login.username.clone(), &conn).unwrap();
+            let user = block_on(conn.get_user(login.username.clone())).unwrap();
             Ok(Json(user))
         }
         Ok(false) => Err(Json(ServerError::NotFound)),
-        Err(db_error) => Err(Json(ServerError::DatabaseError {
-            message: db_error.to_string(),
-        })),
+        Err(db_error) => Err(Json(db_error)),
     }
-}
-
-fn check_password(login: &LoginRequest, conn: &DbConn) -> Result<bool, rusqlite::Error> {
-    use pbkdf2::pbkdf2_check;
-    let stmt = "SELECT password FROM user WHERE username = :username";
-
-    let mut stmt = conn.prepare(&stmt)?;
-    let password_hash: String = stmt
-        .query_map_named(&[(":username", &login.username)], |row| row.get(0))?
-        .nth(0)
-        .unwrap()?;
-
-    Ok(pbkdf2_check(&login.password, &password_hash).is_ok())
 }
 
 /// Retrieve the user's ID, if any.
@@ -609,12 +356,16 @@ fn main() {
 
     init_logger();
 
+    info!("Creating database pool");
+    let pool = task::block_on(db::Pool::new("data/database.sqlite")).unwrap();
+
     // Launch the websocket server
     let websocket_server = websocket::prepare_websocket();
 
     // Launch the regular webserver
     rocket::ignite()
         .manage(websocket_server.clone())
+        .manage(pool)
         .attach(AdHoc::on_attach("Websocket Config", |rocket| {
             let websocket_port: u16 =
                 rocket.config().get_int("websocket_port").unwrap_or(1111) as u16;
@@ -623,7 +374,6 @@ fn main() {
 
             Ok(rocket.manage(WebsocketPort(websocket_port)))
         }))
-        .attach(DbConn::fairing())
         .attach(AdHoc::on_request("Request Logger", |req, _| {
             info!("Request started for: {}", req.uri());
         }))
@@ -643,11 +393,6 @@ fn main() {
                 position_get,
                 random_position,
                 analyse_position,
-                article_get_my_list,
-                article_get_public_list,
-                article_get,
-                article_post,
-                article_post_visible,
                 share,
                 create_game,
                 get_game,
