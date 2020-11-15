b03b1bf6c90f0d3be2f3090ee8951da4817ad342
Rolf Kreibaum <rolf.kreibaum@posteo.de>
2020-11-15 09:22:29 +0100

    Update to chat server example.

--- backend/src/ws2.rs
+++ backend/src/ws2.rs
@@ -1,65 +1,106 @@
 //! Setting up a paralell websocket server. This should help me integrate
 //! the new tungstenite server and work on features for it.
 
-use std::thread;
-
 use async_std::net::{TcpListener, TcpStream};
 use async_std::task;
 use async_tungstenite;
-use async_tungstenite::WebSocketStream;
+use async_tungstenite::tungstenite::protocol::Message;
 use futures::prelude::*;
+use futures::{
+    channel::mpsc::{unbounded, UnboundedSender},
+    future, pin_mut,
+};
 use log::info;
+use std::{
+    collections::HashMap,
+    io::Error as IoError,
+    net::SocketAddr,
+    sync::{Arc, Mutex},
+    thread,
+};
 
-async fn run(port: u16) -> Result<(), std::io::Error> {
+type Tx = UnboundedSender<Message>;
+type PeerMap = Arc<Mutex<HashMap<SocketAddr, Tx>>>;
+
+async fn run(port: u16) -> Result<(), IoError> {
     let addr = format!("0.0.0.0:{}", port);
 
     // Create the event loop and TCP listener we'll accept connections on.
-    let try_socket = TcpListener::bind(&addr).await;
-    let listener = try_socket.expect("Failed to bind");
+    let listener = TcpListener::bind(&addr).await?;
     info!("Ws2 is listening on: {}", addr);
 
+    let state = PeerMap::new(Mutex::new(HashMap::new()));
+
     while let Ok((stream, _)) = listener.accept().await {
-        task::spawn(accept_connection(stream));
+        task::spawn(accept_connection(state.clone(), stream));
     }
 
     Ok(())
 }
 
-async fn accept_connection(stream: TcpStream) {
-    let addr = stream
-        .peer_addr()
-        .expect("connected streams should have a peer address");
+async fn accept_connection(
+    peer_map: PeerMap,
+    stream: TcpStream,
+) -> async_tungstenite::tungstenite::Result<()> {
+    let addr = stream.peer_addr()?;
     info!("Peer address: {}", addr);
 
+    // Get the websocket stream and split it.
     let ws_stream = async_tungstenite::accept_async(stream)
         .await
         .expect("Error during the websocket handshake occurred");
+    let (write, read) = ws_stream.split();
+
+    // Create a channel that we can put into the peer map. When we receive
+    // messages over this channel, we pass it to our client.
+    let (tx, rx) = unbounded();
+    peer_map.lock().unwrap().insert(addr, tx);
+
+    let receive_from_others = rx.map(Ok).forward(write);
+    pin_mut!(receive_from_others);
 
     info!("New WebSocket connection: {}", addr);
-    echo_connection(ws_stream).await;
-    info!("Connection closed: {}", addr);
-}
+    // handle_connection(ws_stream).await?;
 
-async fn echo_connection(
-    ws_stream: WebSocketStream<TcpStream>,
-) -> async_tungstenite::tungstenite::Result<()> {
-    let (mut write, mut read) = ws_stream.split();
+    let broadcast_incoming = read
+        .try_filter(|msg| {
+            // Broadcasting a Close message from one client
+            // will close the other clients.
+            future::ready(!msg.is_close())
+        })
+        .try_for_each(|msg| {
+            info!(
+                "Received a message from {}: {}",
+                addr,
+                msg.to_text().unwrap(),
+            );
+            let peers = peer_map.lock().unwrap();
 
-    loop {
-        match read.next().await {
-            Some(msg) => {
-                let msg = msg?;
-                if msg.is_text() || msg.is_binary() {
-                    info!("msg: {:?}", msg);
-                    write.send(msg).await?;
-                } else if msg.is_close() {
-                    break;
+            // We want to broadcast the message to everyone except ourselves.
+            let broadcast_recipients = peers.iter().filter(|(peer_addr, _)| peer_addr != &&addr);
+
+            for (peer_addr, recp) in broadcast_recipients {
+                if let Err(_) = recp.unbounded_send(msg.clone()) {
+                    warn!(
+                        "Closed Receiver not properly removed from map: {}",
+                        peer_addr
+                    );
                 }
             }
-            None => break, // terminated
-        }
-    }
 
+            future::ok(())
+        });
+
+    pin_mut!(broadcast_incoming);
+    // Wait for one of the futures to complete. The receive_from_others should
+    // stay available all the time, so I expect this to only resolve when
+    // broadcast_incoming resolves.
+    future::select(broadcast_incoming, receive_from_others).await;
+
+    info!("Connection closed: {}", addr);
+    peer_map.lock().unwrap().remove(&addr);
+
+    // Websocket terminated successfully.
     Ok(())
 }
 
