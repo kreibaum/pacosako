da0d8c8fff2871196ce80d30f8d6a1164729425b
Rolf Kreibaum <rolf.kreibaum@posteo.de>
2021-02-06 12:20:21 +0100

    Add some more random generators for puzzles.

--- lib/examples/rng.rs
+++ lib/examples/rng.rs
@@ -44,13 +44,66 @@ fn main() -> Result<(), PacoError> {
         // let max_chain_length: usize = max(max_white, max_black);
         // let min_chain_length: usize = max(min_white, min_black);
 
-        if let Some(_) = puzzle_book_for_children(&sequences) {
+        // if let Some(_) = puzzle_book_for_children(&sequences) {
+        //     println!("{}", board);
+        // }
+
+        if let Some(info) = long_chains_for_discord(&sequences) {
+            println!("{}", info);
             println!("{}", board);
         }
 
-        if counter >= 1000 {
+        // if let Some(info) = many_start_positions(&sequences) {
+        //     println!("{}", info);
+        //     println!("{}", board);
+        // }
+
+        if counter >= 1000000 {
             return Ok(());
         }
+    }
+}
+
+fn long_chains_for_discord(sequences: &SakoSearchResult) -> Option<String> {
+    let shortest_sequence_white = sequences.white.iter().map(|o| o.len()).min().unwrap_or(0);
+    let shortest_sequence_black = sequences.black.iter().map(|o| o.len()).min().unwrap_or(0);
+
+    let no_promotion = !sequences.white.iter().any(chain_contains_promotion)
+        && !sequences.black.iter().any(chain_contains_promotion);
+
+    if (shortest_sequence_white >= 15 || shortest_sequence_black >= 15) && no_promotion {
+        Some(format!(
+            "w: {}, b: {}",
+            shortest_sequence_white, shortest_sequence_black
+        ))
+    } else {
+        None
+    }
+}
+
+/// Puzzles with multiple solutions that avoid promoting in chains
+fn many_start_positions(sequences: &SakoSearchResult) -> Option<String> {
+    let white_has_direct_capture = sequences.white.iter().any(|chain| chain.len() <= 2);
+    let black_has_direct_capture = sequences.black.iter().any(|chain| chain.len() <= 2);
+
+    if (white_has_direct_capture || black_has_direct_capture) {
+        return None;
+    }
+
+    let total_sequences = sequences.black.len() + sequences.white.len();
+    let no_promotion = !sequences.white.iter().any(chain_contains_promotion)
+        && !sequences.black.iter().any(chain_contains_promotion);
+    let total_starting_points = starting_points(sequences).len();
+
+    if total_sequences >= 5
+        && total_starting_points >= 7
+        && !white_has_direct_capture
+        && !black_has_direct_capture
+        && no_promotion
+    {
+        Some(format!("starting points: {}", total_starting_points))
+    } else {
+        None
     }
 }
 
