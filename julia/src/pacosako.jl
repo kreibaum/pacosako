61045e82512eea4b06bfbd1fdaf7180b810c4243
Rolf Kreibaum <rolf.kreibaum@posteo.de>
2021-05-12 21:35:59 +0200

    Make PacoSako usable from julia code.

--- julia/src/pacosako.jl
+++ julia/src/pacosako.jl
@@ -1,11 +1,15 @@
 """This module wraps the rust pacosako dynamic library"""
 
-# using Jtac
+using Jtac
 
-const DYNLIB_PATH = "../lib/target/debug/libpacosako.so"
+# To build this, run `cargo build` in ../lib
+# const DYNLIB_PATH = "../lib/target/debug/libpacosako.so"
+# To build this, run `cargo build --release` in ../lib
+const DYNLIB_PATH = "../lib/target/release/libpacosako.so"
 
-mutable struct PacoSako # <: Game # We need to be using Jtac to subtype Game.
+mutable struct PacoSako <: Game.AbstractGame
     ptr::Ptr{Nothing}
+    forfeit_by::Int64
 end
 
 
@@ -16,7 +20,7 @@ function PacoSako()::PacoSako
 end
 
 function wrap_pacosako_ptr(ptr::Ptr{Nothing})::PacoSako
-    ps = PacoSako(ptr)
+    ps = PacoSako(ptr, 0)
     finalizer(destroy!, ps)
     ps
 end
@@ -34,24 +38,47 @@ end
 
 function Base.copy(ps::PacoSako)::PacoSako
     ptr = ccall((:clone, DYNLIB_PATH), Ptr{Nothing}, (Ptr{Nothing},), ps.ptr)
-    wrap_pacosako_ptr(ptr)
+    ps2 = wrap_pacosako_ptr(ptr)
+    ps2.forfeit_by = ps.forfeit_by
+    ps2
 end
 
-# TODO: this one next.
-# status(ps::PacoSako) = ccall((:status, DYNLIB_PATH), Int64, (Ptr{Nothing},), ps.ptr)
+function Game.status(ps::PacoSako)::Int64
+    if ps.forfeit_by != 0
+        -ps.forfeit_by
+    else
+        ccall((:status, DYNLIB_PATH), Int64, (Ptr{Nothing},), ps.ptr)
+    end
+end
 
-current_player(ps::PacoSako)::Int64 = ccall((:current_player, DYNLIB_PATH), Int64, (Ptr{Nothing},), ps.ptr)
+Game.current_player(ps::PacoSako)::Int64 = ccall((:current_player, DYNLIB_PATH), Int64, (Ptr{Nothing},), ps.ptr)
 
-function legal_actions(ps::PacoSako)
+function Game.legal_actions(ps::PacoSako)
     out = zeros(UInt8, 128)
     ccall((:legal_actions, DYNLIB_PATH), Nothing, (Ptr{Nothing}, Ptr{UInt8}), ps.ptr, out)
     Int.(collect(Iterators.takewhile(x -> x > 0, out)))
 end
 
-function apply_action!(ps::PacoSako, action::Int)::PacoSako
+function Game.apply_action!(ps::PacoSako, action::Int)::PacoSako
     status_code = ccall((:apply_action_bang, DYNLIB_PATH), Int64, (Ptr{Nothing}, UInt8), ps.ptr, UInt8(action))
+    if length(Game.legal_actions(ps)) == 0
+        ps.forfeit_by = Game.current_player(ps)
+    end
     ps
 end
+
+## TODOS:
+
+# Only when we want to to use neuronal network models
+# Game.array(:: AbstractGame) = error("unimplemented")
+
+Game.policy_length(:: Type{PacoSako})::Int = 132
+
+# Only when a human player wants to play
+# draw(io :: IO, game :: AbstractGame) :: Nothing = error("drawing $(typeof(game)) not implemented.")
+
+# For performance:
+# function is_action_legal(game :: AbstractGame, action :: ActionIndex)
 
 ################################################################################
 ## Helpers #####################################################################
--- lib/src/export.rs
+++ lib/src/export.rs
@@ -43,6 +43,7 @@ pub extern "C" fn current_player(ps: *mut DenseBoard) -> i64 {
 /// no null termination is used.
 #[no_mangle]
 pub extern "C" fn legal_actions(ps: *mut DenseBoard, mut out: *mut u8) {
+    use crate::PieceType::*;
     let ps: &mut DenseBoard = unsafe { &mut *ps };
     if let Ok(ls) = ps.actions() {
         let mut length = 0;
@@ -50,7 +51,11 @@ pub extern "C" fn legal_actions(ps: *mut DenseBoard, mut out: *mut u8) {
             let a = match action {
                 crate::PacoAction::Lift(p) => 1 + p.0,
                 crate::PacoAction::Place(p) => 1 + p.0 + 64,
-                crate::PacoAction::Promote(_) => 129,
+                crate::PacoAction::Promote(Rook) => 129,
+                crate::PacoAction::Promote(Knight) => 130,
+                crate::PacoAction::Promote(Bishop) => 131,
+                crate::PacoAction::Promote(Queen) => 132,
+                crate::PacoAction::Promote(_) => 255,
             };
             unsafe {
                 *out = a;
@@ -68,12 +73,25 @@ pub extern "C" fn legal_actions(ps: *mut DenseBoard, mut out: *mut u8) {
 
 #[no_mangle]
 pub extern "C" fn apply_action_bang(ps: *mut DenseBoard, action: u8) -> i64 {
+    use crate::PieceType::*;
     let ps: &mut DenseBoard = unsafe { &mut *ps };
     let res = if 1 <= action && action <= 64 {
         let action = crate::PacoAction::Lift(BoardPosition(action - 1));
         ps.execute(action)
     } else if action <= 128 {
         let action = crate::PacoAction::Place(BoardPosition(action - 1 - 64));
+        ps.execute(action)
+    } else if action == 129 {
+        let action = crate::PacoAction::Promote(Rook);
+        ps.execute(action)
+    } else if action == 130 {
+        let action = crate::PacoAction::Promote(Knight);
+        ps.execute(action)
+    } else if action == 131 {
+        let action = crate::PacoAction::Promote(Bishop);
+        ps.execute(action)
+    } else if action == 132 {
+        let action = crate::PacoAction::Promote(Queen);
         ps.execute(action)
     } else {
         return -1;
@@ -82,5 +100,18 @@ pub extern "C" fn apply_action_bang(ps: *mut DenseBoard, action: u8) -> i64 {
         -1
     } else {
         0
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn status(ps: *mut DenseBoard) -> i64 {
+    use crate::PlayerColor::*;
+    let ps: &mut DenseBoard = unsafe { &mut *ps };
+    match ps.victory_state {
+        crate::VictoryState::Running => 42,
+        crate::VictoryState::PacoVictory(White) => 1,
+        crate::VictoryState::PacoVictory(Black) => -1,
+        crate::VictoryState::TimeoutVictory(White) => 1,
+        crate::VictoryState::TimeoutVictory(Black) => -1,
     }
 }
