b975dca57a4ec1facb1e0bf110bd76c7ffa20de4
Rolf Kreibaum <rolf.kreibaum@posteo.de>
2023-08-05 00:11:43 +0200

    Fix service setup, improve error logging.

--- backend/src/config.rs
+++ backend/src/config.rs
@@ -18,23 +18,40 @@ pub struct EnvironmentConfig {
 /// If no argument is provided, it will load the default config file. This is
 /// useful for development.
 pub fn load_config() -> EnvironmentConfig {
+    match load_config_inner() {
+        Ok(config) => config,
+        Err(err) => {
+            // With the immediate exit, we can't use error!() here.
+            println!("Error loading config: {}", err);
+            std::process::exit(1);
+        }
+    }
+}
+
+/// Inner method to unify error handling
+fn load_config_inner() -> Result<EnvironmentConfig, String> {
     // Get the command line arguments
     let args: Vec<String> = env::args().collect();
 
     let config_filename = match args.len() {
-        1 => "dev-server.toml".to_string(),
+        1 => "dev-config.toml".to_string(),
         2 => args[1].clone(),
         _ => {
-            println!("Usage: {} [config_file]", args[0]);
-            std::process::exit(1);
+            return Err(format!("Usage: {} [config_file]", args[0]));
         }
     };
 
     // Read the config file
     let config_file = fs::read_to_string(&config_filename)
-        .unwrap_or_else(|_| panic!("Could not read config file at path: {}", config_filename));
+        .map_err(|_| format!("Could not read config file at path: {}", config_filename))?;
+
+    info!("Loaded config file: {}", config_filename);
 
     // Parse the config file
-    toml::from_str(&config_file)
-        .unwrap_or_else(|_| panic!("Could not parse config file at path: {}", config_filename))
+    toml::from_str(&config_file).map_err(|e| {
+        format!(
+            "Could not parse config file at path: {}\nCaused by: {:?}",
+            config_filename, e
+        )
+    })
 }
