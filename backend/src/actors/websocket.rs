b4fa4ef8b58be358edac646591799a5747f71def
Rolf Kreibaum <rolf.kreibaum@posteo.de>
2023-08-06 20:31:14 +0200

    Chang API of websocket module and improve logging.

--- backend/src/actors/websocket.rs
+++ backend/src/actors/websocket.rs
@@ -6,7 +6,6 @@ use axum::{
         WebSocketUpgrade,
     },
     response::Response,
-    Error,
 };
 use dashmap::DashMap;
 use futures_util::{
@@ -19,65 +18,66 @@ use tokio::{
     task::AbortHandle,
 };
 
+/// Identifies a websocket connection across the server.
+/// This is used to send messages to a specific socket.
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub struct SocketId(usize);
+
+/// Handler for websocket connections to be used in axum routes.
 pub async fn websocket_handler(ws: WebSocketUpgrade) -> Response {
     ws.on_upgrade(handle_socket)
 }
 
-// All Sockets must be stored in this structure so everyone can send messages to
-// everyone else.
-struct AllSockets {
-    map: DashMap<SocketId, SocketData>,
-    next_id: atomic::AtomicUsize,
-}
-
-impl AllSockets {
-    pub fn generate_id() -> SocketId {
-        SocketId(ALL_SOCKETS.next_id.fetch_add(1, atomic::Ordering::Relaxed))
+/// Lot's of static methods to interact with the AllSockets instance.
+impl SocketId {
+    /// Generates a new unique socket id to be used to refer to a socket.
+    pub fn new() -> SocketId {
+        SocketId(NEXT_ID.fetch_add(1, atomic::Ordering::Relaxed))
     }
-    // Insert a new socket into the map
-    pub fn insert_new(id: SocketId, data: SocketData) {
-        ALL_SOCKETS.map.insert(id, data);
+    /// Calling this method makes the socket callable by it's id.
+    fn with_data(self, data: SocketData) {
+        ALL_SOCKETS.insert(self, data);
     }
-    // If there is a socket registered with this id, return a Sender that can be
-    // used to send messages to it. This sender puts messages into a mpsc queue
-    // that is processed by the socket's task.
-    pub fn sender(id: SocketId) -> Option<Sender<Message>> {
-        let entry = ALL_SOCKETS.map.get(&id)?;
+    /// If there is a socket registered with this id, return a Sender that can
+    /// be used to send messages to it. This sender puts messages into a mpsc
+    /// queue that is processed by the socket's task.
+    pub fn sender(self) -> Option<Sender<Message>> {
+        let entry = ALL_SOCKETS.get(&self)?;
         Some(entry.to_client.clone())
     }
-    // Send a message to a given socket id. This is fire and forget. If the
-    // message can't be sent, it is silently dropped. Use sender(id) for more
-    // control.
-    pub async fn send(id: SocketId, msg: Message) {
-        if let Some(sender) = Self::sender(id) {
+    /// Send a message to a given socket id. This is fire and forget. If the
+    /// message can't be sent, it is silently dropped. Use sender(id) for more
+    /// control.
+    pub async fn send(self, msg: Message) {
+        if let Some(sender) = self.sender() {
             if let Err(e) = sender.send(msg).await {
                 error!(
                     "Internal error queuing message for websocket {}: {}",
-                    id.0, e
+                    self.0, e
                 );
             }
         }
     }
-    // Remove a socket from the map. This aborts the socket's tasks.
-    pub fn remove(id: SocketId) {
-        if let Some((_, data)) = ALL_SOCKETS.map.remove(&id) {
-            info!("Removing websocket {} and aborting tasks", id.0);
+    /// Remove a socket from the map. This aborts the socket's tasks.
+    pub fn remove(self) {
+        if let Some((_, data)) = ALL_SOCKETS.remove(&self) {
+            let remaining_sockets = ALL_SOCKETS.len();
+            info!(
+                "Removing websocket {} and aborting tasks. There are {} remaining connections.",
+                self.0, remaining_sockets
+            );
             data.writer_task_abort_handle.abort();
             data.reader_task_abort_handle.abort();
         }
     }
 }
 
-// Global AllSockets instance:
 lazy_static! {
-    static ref ALL_SOCKETS: AllSockets = AllSockets {
-        map: DashMap::new(),
-        next_id: atomic::AtomicUsize::new(1)
-    };
+    /// All Sockets must be stored in this map so everyone can send messages to
+    /// everyone else.
+    static ref ALL_SOCKETS: DashMap<SocketId, SocketData> = DashMap::new();
+    static ref NEXT_ID: atomic::AtomicUsize = atomic::AtomicUsize::new(1);
 }
-
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
-struct SocketId(usize);
 
 struct SocketData {
     to_client: Sender<Message>,
@@ -85,12 +85,14 @@ struct SocketData {
     reader_task_abort_handle: AbortHandle,
 }
 
-pub async fn handle_socket(socket: WebSocket) {
+/// Handles a new websocket connection, setting up the tasks that read and
+/// write to the socket. This also registers the socket on its id.
+async fn handle_socket(socket: WebSocket) {
     let (sender, receiver) = socket.split();
 
     let (tx, rx) = tokio::sync::mpsc::channel(32);
 
-    let id = AllSockets::generate_id();
+    let id = SocketId::new();
 
     let writer_task_abort_handle = tokio::spawn(write(sender, rx, id)).abort_handle();
     let reader_task_abort_handle = tokio::spawn(read(receiver, id)).abort_handle();
@@ -101,25 +103,40 @@ pub async fn handle_socket(socket: WebSocket) {
         reader_task_abort_handle,
     };
 
-    AllSockets::insert_new(id, socket_data);
+    id.with_data(socket_data);
 }
 
-async fn read(mut receiver: SplitStream<WebSocket>, id: SocketId) -> Result<(), Error> {
+/// Reads all incoming messages from the websocket and delegates them.
+async fn read(mut receiver: SplitStream<WebSocket>, id: SocketId) {
     while let Some(msg) = receiver.next().await {
-        AllSockets::send(id, msg?).await;
+        match msg {
+            Ok(msg) => id.send(msg).await,
+            Err(e) => {
+                error!("Error reading from websocket {}: {}", id.0, e);
+                id.remove();
+                return;
+            }
+        }
     }
-    info!("Websocket receiver task finished.");
-    AllSockets::remove(id);
-    Ok(())
+    info!(
+        "Websocket (id={}) receiver task finished without error.",
+        id.0
+    );
+    id.remove();
 }
 
-// Copies messages from the receiver mpsc to the sender websocket sink.
+/// Copies messages from the receiver mpsc to the sender websocket sink.
 async fn write(mut sender: SplitSink<WebSocket, Message>, mut rx: Receiver<Message>, id: SocketId) {
     while let Some(msg) = rx.recv().await {
-        let Ok(()) = sender.send(msg).await else {
-            break;
+        if let Err(e) = sender.send(msg).await {
+            error!("Error writing to websocket {}: {}", id.0, e);
+            id.remove();
+            return;
         };
     }
-    info!("Websocket sender task finished. Aborting connection.");
-    AllSockets::remove(id);
+    info!(
+        "Websocket (id={}) sender task finished without error.",
+        id.0
+    );
+    id.remove();
 }
