50019187ed966df90b0faafe525f28ab4dde4d42
Rolf Kreibaum <rolf.kreibaum@posteo.de>
2023-09-30 11:42:42 +0200

    Hide replay data for now

--- backend/src/replay_data.rs
+++ backend/src/replay_data.rs
@@ -1,6 +1,6 @@
 //! Module for managing `ReplayMetaInformation` in the backend
 
-use axum::{Json, extract::Path};
+use axum::{extract::Path, Json};
 use serde::Serialize;
 
 #[derive(Serialize)]
@@ -12,40 +12,45 @@ pub struct ReplayMetaData {
     data: String,
 }
 
-
-
 // Get the replay information
 pub async fn get_metadata(Path(key): Path<String>) -> Json<Vec<ReplayMetaData>> {
+    // Disabled for now to make it easy to merge this into the main branch
+    if 2 * 2 > 3 {
+        return Json(vec![]);
+    }
     Json(vec![
         ReplayMetaData {
-            game : key.clone(),
-            action_index : 2,
+            game: key.clone(),
+            action_index: 2,
             category: "Example Arrow".to_owned(),
-            data: r##"{"type":"arrow", "tail":11, "head":27, "color": "#ffc80080", "width" : 20 }"##.to_owned()
+            data:
+                r##"{"type":"arrow", "tail":11, "head":27, "color": "#ffc80080", "width" : 20 }"##
+                    .to_owned(),
         },
         ReplayMetaData {
-            game : key.clone(),
-            action_index : 2,
+            game: key.clone(),
+            action_index: 2,
             category: "Example Arrow".to_owned(),
-            data: r##"{"type":"arrow", "tail":1, "head":18, "color": "#ff0000A0", "width" : 7  }"##.to_owned()
+            data: r##"{"type":"arrow", "tail":1, "head":18, "color": "#ff0000A0", "width" : 7  }"##
+                .to_owned(),
         },
         ReplayMetaData {
-            game : key.clone(),
-            action_index : 4,
+            game: key.clone(),
+            action_index: 4,
             category: "Weight Arrow".to_owned(),
-            data: r##"{"type":"arrow", "tail":13, "head":21, "weight" : 1 }"##.to_owned()
+            data: r##"{"type":"arrow", "tail":13, "head":21, "weight" : 1 }"##.to_owned(),
         },
         ReplayMetaData {
-            game : key.clone(),
-            action_index : 4,
+            game: key.clone(),
+            action_index: 4,
             category: "Weight Arrow".to_owned(),
-            data: r##"{"type":"arrow", "tail":14, "head":22, "weight" : 2 }"##.to_owned()
+            data: r##"{"type":"arrow", "tail":14, "head":22, "weight" : 2 }"##.to_owned(),
         },
         ReplayMetaData {
-            game : key.clone(),
-            action_index : 4,
+            game: key.clone(),
+            action_index: 4,
             category: "Weight Arrow".to_owned(),
-            data: r##"{"type":"arrow", "tail":15, "head":23, "weight" : 3 }"##.to_owned()
-        }
+            data: r##"{"type":"arrow", "tail":15, "head":23, "weight" : 3 }"##.to_owned(),
+        },
     ])
 }
