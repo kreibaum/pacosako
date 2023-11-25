03a473694162cbe4776c51cc67eb321535b2d483
Thomas Staudt <tscode@posteo.net>
2023-11-25 10:06:25 +0100

    first batch of changes to adapt to jtac version 0.2

--- julia/src/JtacPacoSako.jl
+++ julia/src/JtacPacoSako.jl
@@ -1,4 +1,22 @@
-"""This module wraps the rust pacosako dynamic library"""
+
+"""
+Module that wraps the rust pacosako dynamic library and makes it accessible
+to the Jtac boardgame training package.
+
+!!! To use this package, you currently have to manually run
+!!! `cargo build --release` in `../lib`.
+
+It provides the type `PacoSako`, which implements the `Jtac.Game.AbstractGame`
+interface. This enables Jtac support for creating classical or neural network
+based AIs, improving neural AIs via self-play (in an Alpha Zero inspired way),
+benchmarking their performance, or playing against them. One classical AI
+model, `Luna`, which relies on a strict analysis for possible checks and uses a
+simple heuristic to propose state values and action policies, is implemented in
+this package.
+
+The submodule `PacoPlay` bridges this package to the pacoplay webpage, and
+exports utility function for playing against a Jtac AI on it.
+"""
 module JtacPacoSako
 
 using Jtac
@@ -14,28 +32,32 @@ export Jtac,
        Player,
        Training
 
+
+export PacoSako, Luna
+export fen, random_position
+
+# TODO: Use the Artifact interface to store compiled versions of the library
+# in the web?
+const DYNLIB_PATH = joinpath(dirname(@__DIR__), "../lib/target/release/libpacosako.so")
+
+"""
+    libcall(name, args...) 
+
+Wrapper around [`ccall`](@ref) that automatically inserts the pacosako shared
+library.
+"""
+libcall(name, args...) = ccall((name, DYNLIB_PATH), args...)
+
+# Jtac.Game.AbstractGame implementation of PacoSako
 include("pacosako.jl")
 
+# Luna ai model
 include("luna.jl")
 
-# PacoSako game type and Luna Model
-export PacoSako,
-       Luna
 
-export fen,
-       random_position
-
-#function __init__()
-#    # We have to register the game in order to use all functionality of Jtac
-#    # (loading and saving datasets and models)
-#    Pack.register(PacoSako)
-#    Pack.register(Luna)
-#end
-
-################################################################################
-## PacoPlay module to interact with pacoplay servers ###########################
-################################################################################
-
+"""
+Glue between the JtacPacoSako package and the pacoplay webpage.
+"""
 module PacoPlay
 
   using ..JtacPacoSako
--- julia/src/pacosako.jl
+++ julia/src/pacosako.jl
@@ -1,226 +1,287 @@
 
-# To build this, run `cargo build` in ../lib
-# const DYNLIB_PATH = "../lib/target/debug/libpacosako.so"
-# To build this, run `cargo build --release` in ../lib
-const DYNLIB_PATH = joinpath(dirname(@__DIR__), "../lib/target/release/libpacosako.so")
-
+"""
+PacoSako game state. Wrapper around the rust type TODO:???.
+"""
 mutable struct PacoSako <: Game.AbstractGame
-    ptr #::Ptr{Nothing}
-    bin::Pack.Bytes # store binary representation if frozen
-    forfeit_by::Int64
+  ptr :: Ptr{Nothing}
+  forfeit_by :: Int64
 end
 
-"""Initializer and general memory management"""
-function PacoSako()::PacoSako
-    ptr = ccall((:new, DYNLIB_PATH), Ptr{Nothing}, ())
-    wrap_pacosako_ptr(ptr)
+function Pack.pack(io :: IO, ps :: PacoSako) :: Nothing
+  Pack.pack(io, Pack.Bytes(serialize(ps))) 
 end
 
-function PacoSako(fen::String)::PacoSako
-    bytes = Vector{UInt8}(fen)
-    ptr = ccall((:parse_fen, DYNLIB_PATH), Ptr{Nothing}, (Ptr{UInt8}, Int64), bytes, length(bytes))
-    @assert ptr != C_NULL "invalid fen string"
-    wrap_pacosako_ptr(ptr)
-end
-
-function wrap_pacosako_ptr(ptr::Ptr{Nothing})::PacoSako
-    @assert ptr != C_NULL
-    ps = PacoSako(ptr, UInt8[], 0)
-    finalizer(destroy!, ps)
-    status = ccall((:status, DYNLIB_PATH), Int64, (Ptr{Nothing},), ps.ptr)
-    if length(Game.legal_actions(ps)) == 0 && status == 42
-        ps.forfeit_by = Game.current_player(ps)
-    end
-    ps
-end
-
-"""Destructor. Never call that manually, or you'll double free use after free."""
-function destroy!(ps::PacoSako)
-    # ccall to rust to tell it to release the object.
-    ccall((:drop, DYNLIB_PATH), Nothing, (Ptr{Nothing},), ps.ptr)
+function Pack.unpack(io :: IO, :: Type{PacoSako}) :: PacoSako
+  bytes = Pack.unpack(io, Pack.Bytes)
+  deserialize(bytes.data)
 end
 
 
-################################################################################
-## Implementation of the Game interface. #######################################
-################################################################################
+"""
+    destroy!(ps)
 
-function Base.copy(ps::PacoSako)::PacoSako
-    @assert !is_frozen(ps)
-    ptr = ccall((:clone, DYNLIB_PATH), Ptr{Nothing}, (Ptr{Nothing},), ps.ptr)
-    wrap_pacosako_ptr(ptr)
+Destructor function for a paco sako game state `ps`. After `destroy!` has been
+called, `ps` must not be used anymore.
+
+This function should never be called manually.
+"""
+function destroy!(ps :: PacoSako)
+  libcall(:drop, Nothing, (Ptr{Nothing},), ps.ptr)
 end
 
-function Game.status(ps::PacoSako)::Int64
-    @assert !is_frozen(ps)
-    if ps.forfeit_by != 0
-        -ps.forfeit_by
-    else
-        ccall((:status, DYNLIB_PATH), Int64, (Ptr{Nothing},), ps.ptr)
-    end
+"""
+    wrap_pacosako_ptr(ptr) 
+
+Wrap the pointer `ptr`, which must point to a valid pacosako object, as
+[`PacoSako`](@ref) struct.
+"""
+function wrap_pacosako_ptr(ptr :: Ptr{Nothing}) :: PacoSako
+  @assert ptr != C_NULL "Cannot wrap a nullpointer as PacoSako state"
+  ps = PacoSako(ptr, UInt8[], 0)
+  finalizer(destroy!, ps)
+  status = libcall(:status, Int64, (Ptr{Nothing},), ps.ptr)
+  if length(Game.legalactions(ps)) == 0 && status == 42
+      ps.forfeit_by = Game.activeplayer(ps)
+  end
+  ps
 end
 
-Game.current_player(ps::PacoSako)::Int64 = ccall((:current_player, DYNLIB_PATH), Int64, (Ptr{Nothing},), ps.ptr)
+"""
+    PacoSako()
+    PacoSako(fen)
 
-function Game.legal_actions(ps::PacoSako)
-    @assert !is_frozen(ps)
-    # While there are 132 possible actions (64+64+4) there can be at most 64
-    # actions that are legal at any point.
-    out = zeros(UInt8, 64)
-    ccall((:legal_actions, DYNLIB_PATH), Nothing, (Ptr{Nothing}, Ptr{UInt8}), ps.ptr, out)
-    Int.(collect(Iterators.takewhile(x -> x > 0, out)))
+Create a new `PacoSako` instance. If no fen-notated string `fen` is provided,
+the returned game is in the default initial state.
+"""
+function PacoSako() :: PacoSako
+  ptr = libcall(:new, Ptr{Nothing}, ())
+  wrap_pacosako_ptr(ptr)
 end
 
-function Game.apply_action!(ps::PacoSako, action::Int)::PacoSako
-    @assert !is_frozen(ps)
-    status_code = ccall((:apply_action_bang, DYNLIB_PATH), Int64, (Ptr{Nothing}, UInt8), ps.ptr, UInt8(action))
-    if length(Game.legal_actions(ps)) == 0 && ccall((:status, DYNLIB_PATH), Int64, (Ptr{Nothing},), ps.ptr) == 42
-        ps.forfeit_by = Game.current_player(ps)
-    end
-    @assert status_code == 0 "Error during apply_action! of PacoSako game"
-    ps
+function PacoSako(fen :: String) :: PacoSako
+  bytes = Vector{UInt8}(fen)
+  ptr = libcall(
+    :parse_fen,
+    Ptr{Nothing},
+    (Ptr{UInt8}, Int64),
+    bytes,
+    length(bytes)
+  )
+  @assert ptr != C_NULL "invalid fen string"
+  wrap_pacosako_ptr(ptr)
 end
 
-function Game.array(pss::Vector{PacoSako})
-    batchsize = length(pss)
-    buf = Game.array_buffer(PacoSako, batchsize)
-    Game.array!(buf, pss)
-    buf
+function Game.status(ps :: PacoSako) :: Int64
+  if ps.forfeit_by != 0
+    -ps.forfeit_by
+  else
+    libcall(:status, Int64, (Ptr{Nothing},), ps.ptr)
+  end
 end
 
-function Game.array(ps::PacoSako)
-    reshape(Game.array([ps]), 8, 8, 30)
+function Game.activeplayer(ps :: PacoSako) :: Int64
+  libcall(:current_player, Int64, (Ptr{Nothing},), ps.ptr)
 end
 
-function Game.array!(buf, pss :: Vector{PacoSako})
-    # do arguments make sense?
-    batchsize = length(pss)
-    @assert size(buf)[1:3] == size(PacoSako)
-    @assert size(buf, 4) >= batchsize
+Game.policylength(::Type{PacoSako})::Int = 132
 
-    # some working memory
-    idxrepr = zeros(UInt32, 38)
-    one_indices = zeros(UInt32, 33, batchsize)
-    layer_vals = zeros(Float32, 5, batchsize)
-
-    for (i, ps) in enumerate(pss)
-        # set idxrepr via call to rust
-        ccall( (:get_idxrepr, DYNLIB_PATH)
-             , Int64, (Ptr{Nothing}, Ptr{Nothing}, Int64)
-             , ps.ptr, idxrepr, length(idxrepr) )
-
-        one_indices[:, i] .= 1 .+ idxrepr[1:33] .+ (i-1) * prod(size(PacoSako))
-        layer_vals[:, i] .= idxrepr[34:38]
-    end
-
-    # reset buffer
-    buf[:, :, :, 1:batchsize] .= 0
-
-    # set one_indices in buffer to ... one!
-    one_indices = reshape(one_indices, :)
-    buf[one_indices] .= 1
-
-    # set layer values of buffer
-    layer_vals = Model.adapt_atype(buf, layer_vals) # maybe load it to GPU
-    layer_vals = reshape(layer_vals, 1, 1, 5, batchsize) # makes broadcasting work
-    buf[:, :, 26:30, 1:batchsize] .= layer_vals
-
-    # scale final layer
-    buf[:, :, 30, 1:batchsize] ./= 100
-
-    nothing
+function Game.legalactions(ps :: PacoSako)
+  # While there are 132 possible actions (64+64+4) there can be at most 64
+  # actions that are legal at any point.
+  out = zeros(UInt8, 64)
+  libcall(:legal_actions, Nothing, (Ptr{Nothing}, Ptr{UInt8}), ps.ptr, out)
+  # TODO: this is not particularly nice code...
+  Int.(collect(Iterators.takewhile(x -> x > 0, out)))
 end
 
-function Base.size(::Type{PacoSako})
-    layer_count = ccall((:repr_layer_count, DYNLIB_PATH), Int64, ())
-    @assert layer_count > 0 "Layer count must be positive"
-    (8, 8, layer_count)
+function Game.move!(ps :: PacoSako, action :: Int) :: PacoSako
+
+  status_code = libcall(
+    :apply_action_bang,
+    Int64,
+    (Ptr{Nothing}, UInt8),
+    ps.ptr,
+    UInt8(action)
+  )
+  @assert status_code == 0 "Error during move!"
+
+  actions = Game.legalactions(ps)
+  raw_status = libcall(:status, Int64, (Ptr{Nothing},), ps.ptr)
+  if length(actions) == 0 && raw_status == 42
+      ps.forfeit_by = Game.activeplayer(ps)
+  end
+
+  ps
 end
 
-Game.policy_length(::Type{PacoSako})::Int = 132
-
-# Only when a human player wants to play
-# draw(io :: IO, game :: PacoSako) :: Nothing = error("drawing $(typeof(game)) not implemented.")
-
-# For performance:
-# function is_action_legal(game :: PacoSako, action :: ActionIndex)
-
-################################################################################
-## (De-)Serialization ##########################################################
-################################################################################
-
-function serialize(ps::PacoSako)::Vector{UInt8}
-    @assert !is_frozen(ps)
-    length = ccall((:serialize_len, DYNLIB_PATH), Int64, (Ptr{Nothing},), ps.ptr)
-    out = zeros(UInt8, length)
-    status_code = ccall((:serialize, DYNLIB_PATH), Int64, (Ptr{Nothing}, Ptr{UInt8}, Int64), ps.ptr, out, length)
-    @assert status_code == 0 "Error during serialization of PacoSako game!"
-    out
+function Game.array(pss :: Vector{PacoSako})
+  batchsize = length(pss)
+  buf = Game.arraybuffer(PacoSako, batchsize)
+  Game.array!(buf, pss)
+  buf
 end
 
-function deserialize(bincode::Vector{UInt8})::PacoSako
-    ptr = ccall((:deserialize, DYNLIB_PATH), Ptr{Nothing}, (Ptr{UInt8}, Int64), bincode, length(bincode))
-    @assert ptr != C_NULL "Deserialization error for PacoSako game!"
-    wrap_pacosako_ptr(ptr)
+function Game.array(ps :: PacoSako)
+  reshape(Game.array([ps]), 8, 8, 30)
 end
 
-is_frozen(ps::PacoSako)::Bool = !isempty(ps.bin.data)
+function Game.array!(buf, games :: Vector{PacoSako})
+  @assert size(buf)[1:3] == size(PacoSako)
+  @assert size(buf, 4) >= length(games)
 
-function Pack.freeze(ps::PacoSako)::PacoSako
-    @assert !is_frozen(ps)
-    PacoSako(0, serialize(ps), ps.forfeit_by)
+  batchsize = length(games)
+  game_length = prod(size(PacoSako))
+
+  # Reset the buffer that will carry the array representation of the games
+  buf[:, :, :, 1:batchsize] .= 0
+
+  # Buffer for storing the index representation of a single game state.
+  #
+  # The index representation is a sparse representation of a paco sako board
+  # via 38 UInt32 values. The first 33 values are used as indices that identify
+  # which entries of the dense game array should be set to 1 (we call this
+  # operation "scattering"). The final 5 values correspond to the values of the
+  # final 5 layers of the dense game array, all of which are constant.
+  #
+  # The crucial advantage of this representation is that only 38*8 bytes have to
+  # be uploaded to the GPU per game state evaluation. Therefore, the amount of
+  # data transfer between the CPU and GPU drops significantly.
+  tmp = zeros(UInt32, 38)
+
+  # Scatter indices that determine the location of 1s in the array representation
+  scatter_indices = zeros(UInt32, 33, batchsize)
+
+  # Values for the final 5 layers in the array representation
+  layer_values = zeros(Float32, 5, batchsize)
+
+  for (index, ps) in enumerate(games)
+
+    # Get the index representation of this game
+    libcall(
+      :get_idxrepr,
+      Int64,
+      (Ptr{Nothing}, Ptr{Nothing}, Int64),
+      ps.ptr,
+      tmp,
+      length(tmp)
+    )
+
+    # Extract scatter indices and layer values for this game
+    offset = (index - 1) * game_length
+    scatter_indices[:, i] .= 1 .+ tmp[1:33] .+ offset
+    layer_values[:, i] .= tmp[34:38]
+  end
+
+  # Set the buffer to 1 at the 33 scatter index locations
+  scatter_indices = reshape(scatter_indices, :)
+  buf[scatter_indices] .= 1
+
+  # Set the 5 constant layers
+  T = Model.arraytype(buf)
+  layer_values = convert(T, layer_values)
+  buf[:, :, 26:30, 1:batchsize] .= reshape(layer_values, 1, 1, 5, :)
+
+  # Scale the final layer
+  buf[:, :, 30, 1:batchsize] ./= 100
+
+  nothing
 end
 
-function Pack.unfreeze(ps::PacoSako)::PacoSako
-    @assert is_frozen(ps)
-    deserialize(ps.bin.data)
+"""
+    random_position()
+
+Returns a random Paco Sako game state that is legal and not yet over.
+"""
+function random_position() :: PacoSako
+  ptr = libcall(:random_position, Ptr{Nothing}, ())
+  @assert ptr != C_NULL "Error in the random generator for PacoSako"
+  wrap_pacosako_ptr(ptr)
 end
 
+Game.randominstance(:: Type{PacoSako}) = random_position()
+
+"""
+    serialize(ps) 
+
+Serialize the paco sako game state `ps`. Returns a byte vector.
+"""
+function serialize(ps :: PacoSako) :: Vector{UInt8}
+  len = libcall(:serialize_len, Int64, (Ptr{Nothing},), ps.ptr)
+  out = zeros(UInt8, len)
+  status_code = libcall(
+    :serialize,
+    Int64,
+    (Ptr{Nothing}, Ptr{UInt8}, Int64),
+    ps.ptr,
+    out,
+    len
+  )
+  @assert status_code == 0 "Error during serialization of PacoSako game"
+  out
+end
+
+"""
+    deserialize(bytes) 
+
+Deserialize a paco sako game state from the byte vector `bytes`.
+"""
+function deserialize(bincode :: Vector{UInt8}) :: PacoSako
+  ptr = libcall(
+    :deserialize,
+    Ptr{Nothing},
+    (Ptr{UInt8}, Int64),
+    bincode,
+    length(bincode)
+  )
+  @assert ptr != C_NULL "Deserialization error for PacoSako game"
+  wrap_pacosako_ptr(ptr)
+end
 
 ################################################################################
 ## Helpers #####################################################################
 ################################################################################
 
-function Base.show(io::IO, game::PacoSako)
-    if Game.is_over(game)
-        print(io, "PacoSako($(Game.status(game)) won)")
-    else
-        print(io, "PacoSako($(Game.current_player(game)) moving)")
-    end
-end
-
-function Base.show(io::IO, ::MIME"text/plain", game::PacoSako)
-    fen_string = PacoPlay.Url.editor(game)
-    if Game.is_over(game)
-        println(io, "PacoSako game with result $(Game.status(game))")
-        print(io, "  link: $fen_string")
-    else
-        println(io, "PacoSako game with player $(Game.current_player(game)) moving")
-        print(io, "  link: $fen_string")
-    end
+function Game.hash(ps :: PacoSako) :: UInt64
+  libcall(:hash, UInt64, (Ptr{Nothing},), ps.ptr)
 end
 
 function Game.draw(ps::PacoSako)
-    @assert !is_frozen(ps)
-    ccall((:print, DYNLIB_PATH), Nothing, (Ptr{Nothing},), ps.ptr)
+  libcall(:print, Nothing, (Ptr{Nothing},), ps.ptr)
 end
 
-function Base.:(==)(ps1::PacoSako, ps2::PacoSako)::Bool
-    ccall((:equals, DYNLIB_PATH), Int64, (Ptr{Nothing}, Ptr{Nothing}), ps1.ptr, ps2.ptr) == 0
+
+function Base.:(==)(ps1 :: PacoSako, ps2 :: PacoSako) :: Bool
+  libcall(:equals, Int64, (Ptr{Nothing}, Ptr{Nothing}), ps1.ptr, ps2.ptr) == 0
 end
 
-function Game.hash(ps::PacoSako)::UInt64
-    @assert !is_frozen(ps)
-    ccall((:hash, DYNLIB_PATH), UInt64, (Ptr{Nothing},), ps.ptr)
+function Base.show(io :: IO, game :: PacoSako)
+  if Game.isover(game)
+    print(io, "PacoSako($(Game.status(game)) won)")
+  else
+    print(io, "PacoSako($(Game.activeplayer(game)) moving)")
+  end
 end
 
-"""
-Returns a random Paco Ŝako position that is legal and still running.
-"""
-function random_position()::PacoSako
-    ptr = ccall((:random_position, DYNLIB_PATH), Ptr{Nothing}, ())
-    @assert ptr != C_NULL "Error in the random generator for PacoSako"
-    wrap_pacosako_ptr(ptr)
+function Base.show(io :: IO, :: MIME"text/plain", game :: PacoSako)
+  pacoplay_editor_url = PacoPlay.Url.editor(game)
+  if Game.isover(game)
+    println(io, "PacoSako($(Game.status(game)) won)")
+    print(io, "  link: $pacoplay_editor_url")
+  else
+    println(io, "PacoSako($(Game.activeplayer(game)) moving)")
+    print(io, "  link: $pacoplay_editor_url")
+  end
+end
+
+function Base.size(:: Type{PacoSako})
+  layer_count = libcall(:repr_layer_count, Int64, ())
+  @assert layer_count > 0 "Layer count must be positive"
+  (8, 8, layer_count)
+end
+
+function Base.copy(ps :: PacoSako) :: PacoSako
+  ptr = libcall(:clone, Ptr{Nothing}, (Ptr{Nothing},), ps.ptr)
+  wrap_pacosako_ptr(ptr)
 end
 
 """
@@ -236,7 +297,7 @@ not returned.
 function find_paco_sequences(ps::PacoSako)::Vector{Vector{Int64}}
     memory = zeros(UInt8, 1000)
 
-    status_code = ccall((:find_paco_sequences, DYNLIB_PATH), Int64,
+    status_code = libcall(:find_paco_sequences, Int64,
         (Ptr{Nothing}, Ptr{UInt8}, Int64),
         ps.ptr, memory, length(memory))
     @assert status_code == 0 "Error when trying to find sequences $(fen(ps))"
@@ -260,7 +321,7 @@ Given a Paco Ŝako position, this finds out if the current player is in Ŝako an
 needs to defend.
 """
 function is_sako_for_other_player(ps::PacoSako)::Bool
-    ccall((:is_sako_for_other_player, DYNLIB_PATH), Bool, (Ptr{Nothing},), ps.ptr)
+    libcall(:is_sako_for_other_player, Bool, (Ptr{Nothing},), ps.ptr)
 end
 
 """
@@ -268,7 +329,7 @@ Given a Paco Ŝako position, count how many tiles can be attacked by the
 current player.
 """
 function my_threat_count(ps::PacoSako)::Int64
-    ccall((:my_threat_count, DYNLIB_PATH), Int64, (Ptr{Nothing},), ps.ptr)
+    libcall(:my_threat_count, Int64, (Ptr{Nothing},), ps.ptr)
 end
 
 """
@@ -278,7 +339,7 @@ Returns the fen string of the `PacoSako` game state `pacosako`.
 """
 function fen(ps::PacoSako)
     tmp = zeros(UInt8, 100)
-    len = ccall((:write_fen, DYNLIB_PATH), Int64, (Ptr{Nothing}, Ptr{UInt8}, Int64), ps.ptr, tmp, length(tmp))
+    len = libcall(:write_fen, Int64, (Ptr{Nothing}, Ptr{UInt8}, Int64), ps.ptr, tmp, length(tmp))
     @assert len != 0 && len <= length(tmp) "fen string did not fit in allocated memory"
     String(@view tmp[1:len])
 end
