8aa72a96ff31b50842f8bd302a64bf982bdbb7e5
Rolf Kreibaum <rolf.kreibaum@posteo.de>
2024-04-12 09:00:35 +0200

    Add --fast mode to compile-wasm.sh for development.

--- scripts/compile-wasm.sh
+++ scripts/compile-wasm.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Compiles the library into a WebAssembly file.
 # It then gets copied to the `target` folder.
 # The javascript wrapper is also copied to the `target` folder and minified.
@@ -11,16 +13,35 @@
 # Supported by default with Firefox 114
 # https://caniuse.com/mdn-api_worker_worker_ecmascript_modules
 
+# Parse command line arguments for --fast option
+FAST_MODE=0
+for arg in "$@"
+do
+    if [ "$arg" = "--fast" ]; then
+        FAST_MODE=1
+    fi
+done
+
 mkdir -p build/frontend-wasm
 mkdir -p target/js
 
-wasm-pack build frontend-wasm --target no-modules --out-name lib --out-dir ../build/frontend-wasm
+# Set wasm-pack build command & brotli options based on fast mode
+if [ $FAST_MODE -eq 1 ]; then
+    echo "Running in fast mode..."
+    WASM_PACK_OPTS="--dev --target no-modules"
+    BROTLI_OPTS="-f --quality=0"
+else
+    WASM_PACK_OPTS="--release --target no-modules"
+    BROTLI_OPTS="-f"
+fi
+
+wasm-pack build frontend-wasm $WASM_PACK_OPTS --out-name lib --out-dir ../build/frontend-wasm
 cp build/frontend-wasm/lib_bg.wasm target/js/lib.wasm
 
 # Minify the javascript wrapper.
-echo "Minifying wasm wapper javascript"
+echo "Minifying wasm wrapper javascript"
 terser build/frontend-wasm/lib.js -o target/js/lib.min.js --compress --mangle
 
 # Pre-compress both files with brotli
-brotli -f target/js/lib.wasm
-brotli -f target/js/lib.min.js
+brotli $BROTLI_OPTS target/js/lib.wasm
+brotli $BROTLI_OPTS target/js/lib.min.js
