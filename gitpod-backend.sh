0ebfb0b3d78a5f8ac20c0fa12b4cd38f4867d521
Rolf Kreibaum <rolf.kreibaum@posteo.de>
2025-03-08 14:46:24 +0100

    Give up on gitpod. Remove outdated gitpod config.

--- gitpod-backend.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Opens a terminal where the backend runs
-
-cd backend
-cargo run
--- gitpod-frontend.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Opens a terminal where the frontend runs
-
-cd frontend
-# Enable English language
-pytrans.py
-elm-live src/Main.elm --no-server -- --output=../target/elm.js
