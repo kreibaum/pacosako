b64d548f447ff5df219cb6f84ee1f23e214f8e87
Rolf Kreibaum <rolf.kreibaum@posteo.de>
2025-08-06 22:00:28 +0200

    Update ort version & make it work with "decide turn"

--- ai-runner-rs/src/main.rs
+++ ai-runner-rs/src/main.rs
@@ -1,18 +1,21 @@
+use ort::execution_providers::CPUExecutionProvider;
 use ort::execution_providers::CUDAExecutionProvider;
 use ort::inputs;
-use ort::session::builder::GraphOptimizationLevel;
 use ort::session::Session;
+use ort::session::builder::GraphOptimizationLevel;
 use ort::value::{Tensor, Value};
+use pacosako::PacoError::MlModelError;
 use pacosako::ai::model_backend::ModelBackend;
 use pacosako::ai::model_evaluation::ModelEvaluation;
-use pacosako::PacoError::MlModelError;
-use pacosako::{fen, DenseBoard, PacoBoard, PacoError};
+use pacosako::{DenseBoard, PacoBoard, PacoError, fen};
+use std::sync::{Arc, Mutex};
 
-struct CudaOrtBackend {
-    session: Session,
+#[derive(Clone)]
+struct OrtBackend {
+    session: Arc<Mutex<Session>>,
 }
 
-impl ModelBackend for CudaOrtBackend {
+impl ModelBackend for OrtBackend {
     async fn evaluate_model(&mut self, board: &DenseBoard) -> Result<ModelEvaluation, PacoError> {
         let input_repr: &mut [f32; 8 * 8 * 30] = &mut [0.; 8 * 8 * 30];
         pacosako::ai::repr::tensor_representation(board, input_repr);
@@ -23,8 +26,11 @@ impl ModelBackend for CudaOrtBackend {
         let input: Tensor<f32> = Value::from_array((input_shape, input_data))
             .map_err(|_| MlModelError("Error building input tensor".to_string()))?;
 
-        let outputs = self
+        let mut session = self
             .session
+            .lock()
+            .map_err(|_| MlModelError("Error locking session".to_string()))?;
+        let outputs = session
             .run(inputs![input])
             .map_err(|_| MlModelError("Error evaluating model".to_string()))?;
 
@@ -48,9 +54,14 @@ impl ModelBackend for CudaOrtBackend {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Execution providers are loaded in the order they are provided until a suitable execution
+    // provider is found.
     ort::init()
         .with_name("Hedwig")
-        .with_execution_providers([CUDAExecutionProvider::default().build()])
+        .with_execution_providers([
+            CUDAExecutionProvider::default().build(),
+            CPUExecutionProvider::default().build(),
+        ])
         .commit()?;
 
     let session = Session::builder()?
@@ -60,7 +71,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let board = DenseBoard::new();
 
-    let mut backend = CudaOrtBackend { session };
+    let mut backend = OrtBackend {
+        session: Arc::new(Mutex::new(session)),
+    };
 
     println!("{:?}", backend.evaluate_model(&board).await?.sorted());
 
@@ -69,6 +82,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         fen::parse_fen("2R3B1/1p3p1p/1A3n2/2p1r3/pPAp1D2/B3P1N1/P2KbPYk/2C2S1R w 0 AHah - -")?;
 
     println!("{:?}", backend.evaluate_model(&board).await?.sorted());
+
+    // Determine a full move on this board state.
+    pacosako::ai::move_decision::decide_turn_intuition(backend, &board, vec![])
+        .await?
+        .into_iter()
+        .for_each(|action| println!("{:?}", action));
 
     Ok(())
 }
